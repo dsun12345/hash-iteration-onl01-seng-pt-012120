3a87e01ffc7dab46df7f4e70eacb8bd73e4e2670
Daniel Sun <danielsun12345@gmail.com>
2020-01-30 03:39:44 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,4 +10,12 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!" if age < 10 
+  else 
+    puts "You are too old for this."
+  end 
+  end
+end 
+  
